203399d1e33e850b681857035cafda66c0fa61f2
TiyaReba <105912721+TiyaReba@users.noreply.github.com>
2022-07-17 23:00:21 +0530

    department selection added

--- ajax.js
+++ ajax.js
@@ -1,11 +1,13 @@
+// var myDetails;
+var p;
 function ajax(){
 // creating an object
 var xhttp = new XMLHttpRequest();
 // eventlistner
 xhttp.onreadystatechange = function(){
    if(this.readyState==4 && this.status==200) {
-    // document.getElementById("demo").innerHTML = this.responseText;
-    let myDetails = JSON.parse(this.responseText);
+   let  myDetails = JSON.parse(this.responseText);
+    
     makeTable(myDetails);
    }
 };
@@ -16,14 +18,44 @@ xhttp.send();
 
 // Table formation
 function makeTable(data){
-    let p = data.shoppinglist;
-    var out ="<table> <tr><th>SlNo</th><th>Name</th><th>Quantity</th><th>Unit</th><th>Department</th><th>Notes</th></tr>";
-    for(let i=0;i<p.length;i++){
+    p = data.shoppinglist;
+    
+    var out = "<select id=mylist onchange= dept()><option  disabled selected>Department</option><option>All</option><option>Soaps</option><option>Vegetables</option><option>Rice</option></select>   <table><tr><th> SlNo.</th><th>Name</th><th>Quantity</th><th>Unit</th><th>Department</th><th>Notes</th></tr>";
+   
+      for(let i=0;i<p.length;i++){
         out +="<tr> <th>" + p[i].SlNo +"</th>" + "<th>" + p[i].Name +"</th>" +"<th>" + p[i].Quantity +"</th>" +"<th>" + p[i].Unit +"</th>" +"<th>" + p[i].Department +"</th>" +"<th>"  + p[i].Notes +"</th>"  +"</tr>";
        }
        out+="</table>";
     document.getElementById("demo").innerHTML =out; 
-    document.getElementById("head").innerHTML ="<h1> Grocery shopping List </h1>"; 
+    
 }
 
+
+function dept() {
     
+    input = document.getElementById("mylist").value;   
+    var out = "<select id=mylist onchange=dept()><option disabled selected>Department</option><option>All</option><option>Soaps</option><option>Vegetables</option><option>Rice</option></select>   <table id=mytable><tr><th>SlNo.</th><th>Name</th><th>Quantity</th><th>Unit</th><th>Department</th><th>Notes</th></tr>";  
+
+    for (let i = 0; i < p.length; i++) {
+        if (input == "Soaps") {
+            if (p[i].Department == 'Soaps') {
+                out +="<tr> <th>" + p[i].SlNo +"</th>" + "<th>" + p[i].Name +"</th>" +"<th>" + p[i].Quantity +"</th>" +"<th>" + p[i].Unit +"</th>" +"<th>" + p[i].Department +"</th>" +"<th>"  + p[i].Notes +"</th>"  +"</tr>";
+            }
+        }
+        else if (input == "Vegetables") {
+            if (p[i].Department == 'Vegetables') {
+                out +="<tr> <th>" + p[i].SlNo +"</th>" + "<th>" + p[i].Name +"</th>" +"<th>" + p[i].Quantity +"</th>" +"<th>" + p[i].Unit +"</th>" +"<th>" + p[i].Department +"</th>" +"<th>"  + p[i].Notes +"</th>"  +"</tr>";
+            }
+        }
+        else if (input == "Rice") {
+            if (p[i].Department == 'Rice') {
+                out +="<tr> <th>" + p[i].SlNo +"</th>" + "<th>" + p[i].Name +"</th>" +"<th>" + p[i].Quantity +"</th>" +"<th>" + p[i].Unit +"</th>" +"<th>" + p[i].Department +"</th>" +"<th>"  + p[i].Notes +"</th>"  +"</tr>";
+            }
+        }
+        else {
+            out +="<tr> <th>" + p[i].SlNo +"</th>" + "<th>" + p[i].Name +"</th>" +"<th>" + p[i].Quantity +"</th>" +"<th>" + p[i].Unit +"</th>" +"<th>" + p[i].Department +"</th>" +"<th>"  + p[i].Notes +"</th>"  +"</tr>";
+        }
+    }
+    out += "</table>";
+    document.getElementById("demo").innerHTML = out;
+}
